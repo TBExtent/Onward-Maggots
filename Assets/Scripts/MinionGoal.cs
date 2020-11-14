87ca3900949f1ad82b0b6221538c1c6564fc986a
TBExtent <jedpreist@gmail.com>
2020-11-14 22:24:08 +0000

    there is an end! (sort of)

---
+++ Assets/Scripts/MinionGoal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionGoal : MonoBehaviour
+{
+    public int target;
+    private int current;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Minion")) {
+            other.gameObject.transform.parent.GetComponent<MinionSpawner>().MinionWasKilled();
+            Destroy(other.gameObject);
+            current++;
+
+            if (current >= target) {
+                Debug.Log("WOOOOOOO");
+            }
+        }
+    }
+}
