9d3291b64a583b90426791a324d62b08106fb838
TBExtent <jedpreist@gmail.com>
2020-11-14 13:33:19 +0000

    tiles

---
+++ Assets/SuperTiled2Unity/Tiled/tileset/colony-other-ready.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="colony-other-ready" tilewidth="16" tileheight="16" tilecount="1105" columns="17">
+ <image source="colony-other-ready.png" width="272" height="1040"/>
+</tileset>
