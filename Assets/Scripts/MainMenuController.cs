90a01182189767aa5141426c05a9dc93f205e24a
Connor Crees <connor.crees@gmail.com>
2020-11-14 22:15:33 +0000

    Main Menu Music
    Added main menu music.
    Added main menu controller.
    Merges credits scene into main menu.
    Removed Credits scene.

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    public static Transform[] menus;
+    public Transform startMenu;
+
+    private void Awake()
+    {
+        menus = new Transform[transform.childCount];
+        for (int i = 0; i < menus.Length; i++)
+        {
+            menus[i] = transform.GetChild(i);
+        }
+
+        SwitchToMenu(startMenu);
+    }
+
+    public void SwitchToMenu(Transform currentMenu)
+    {
+        currentMenu.gameObject.SetActive(true);
+        for (int i = 0; i < menus.Length; i++)
+        {
+            if(menus[i] != currentMenu)
+            {
+                menus[i].gameObject.SetActive(false);
+            }
+        }
+    }
+}
