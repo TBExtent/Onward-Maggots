d2dc089aaa1cf13604f0db2435ee589725ddc890
Anna <gr18578@bristol.ac.uk>
2020-11-14 23:17:13 +0000

    Allow towers to do damage to player

--- Assets/Scripts/PlayerScripts/PlayerHealthController.cs
+++ Assets/Scripts/PlayerScripts/PlayerHealthController.cs
@@ -5,11 +5,32 @@ using UnityEngine.UI;
 
 public class PlayerHealthController : MonoBehaviour
 {
-    public float playerHealth;
+    public float playerHealth = 2500;
     [SerializeField] private Text healthText;
+
+    private void Start()
+    {
+        UpdateHealth();
+    }
 
     public void UpdateHealth()
     {
         healthText.text = playerHealth.ToString("0");
+    }
+
+    public void doDamage(int damage) 
+    {
+        playerHealth -= damage;
+
+        if (playerHealth <= 0) {
+            playerHealth = 0;
+            UpdateHealth();
+            Destroy(gameObject);
+        } 
+        else 
+        {
+            UpdateHealth();
+        }
+
     }
 }
