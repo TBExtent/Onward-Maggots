9f9819d95c11898e8e10da1dd9fa8f198b361783
Unknown <matthew.crees@ntlworld.com>
2020-11-15 09:26:10 +0000

    Minion noises

---
+++ Assets/Scripts/MinionDeathNoiseController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionDeathNoiseController : MonoBehaviour
+{
+    public GameObject[] Clips;
+
+    public void minionHasDied() {
+        int i = Random.Range(0, Clips.Length);
+        Clips[i].GetComponent<AudioSource>().Play();
+    }
+}
